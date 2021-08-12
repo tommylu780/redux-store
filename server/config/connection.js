b72d023c94b586abee80566ddbfd880cbc541171
tommylu780 <nhoksun340@gmail.com>
2021-08-12 17:11:41 +1000

    connect database to mongodb Atlas

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://tommy-lu:10071995Ltt@cluster0.mg6ta.mongodb.net/mernshopping', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
